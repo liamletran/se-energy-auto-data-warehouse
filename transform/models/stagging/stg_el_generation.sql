3c32d0426be1e8c9dd652dec608ca2a90f4e5884
LLT <liamletran@outlook.com>
2026-05-04 15:38:05 +0200

    added type casting, timezome standardization, deduplication to generation model

--- transform/models/stagging/stg_el_generation.sql
+++ transform/models/stagging/stg_el_generation.sql
@@ -1,12 +1,16 @@
-WITH generation_raw AS (
+WITH source AS (
     SELECT * FROM {{ source('warehouse', 'generation_raw') }}
+), generation_raw AS (
+    SELECT 
+        CAST(datetime AS timestamptz) AT TIME ZONE 'UTC' AS generation_datetime_utc,
+        EXTRACT(HOUR FROM datetime) AS generation_hour,
+        production_type,  
+        COALESCE(actual_generation_mw, 0) AS actual_generation_mw,
+        bidding_zone AS bidding_zone_id,
+        ingested_at AS generation_ingested_at
+    FROM source
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY generation_datetime_utc, production_type, bidding_zone_id ORDER BY generation_datetime_utc) = 1
 )
-SELECT 
-    datetime AS generation_at,
-    CAST(datetime AS DATE) AS generation_date,
-    EXTRACT(HOUR FROM datetime) AS generation_hour,
-    production_type,  
-    COALESCE(actual_generation_mw, 0) AS actual_generation_mw,
-    bidding_zone AS bidding_zone_id,
-    ingested_at AS generation_ingested_at
+
+SELECT * 
 FROM generation_raw
